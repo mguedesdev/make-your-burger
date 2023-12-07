f7cef49df808bf80b69969b374ecec3c21ae4a20
mguedesdev <mathheus.gr@gmail.com>
2023-12-07 11:32:11 -0300

    Passando dados do back para o front

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,16 +2,18 @@
   <div class="main-container">
     <Banner />
     <h1>Monte o seu burger:</h1>
-    <p>formulario</p>
+    <BurgerForm />
   </div>
 </template>
 
 <script>
   import Banner from '../components/Banner.vue';
+  import BurgerForm from '../components/BurgerForm.vue';
   export default {
     name: 'HomeView',
     components: {
-      Banner
+      Banner,
+      BurgerForm
     }
   }
 </script>
